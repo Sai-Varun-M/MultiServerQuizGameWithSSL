0e70e83aab2a14e733a1841fa2772cc6ccfd25fe
Sai-Varun-M <143157157+Sai-Varun-M@users.noreply.github.com>
2024-03-05 20:30:43 +0530

    Final Code for multi client quiz game in python

---
+++ Questions.py
@@ -0,0 +1,6 @@
+Q_and_A = [
+    ("What is the capital of France?", ["London", "Paris", "Berlin", "Rome"], "Paris"),
+    ("Which planet is known as the Red Planet?", ["Mars", "Venus", "Jupiter", "Saturn"], "Mars"),
+    ("Who painted the Mona Lisa?", ["Vincent van Gogh", "Leonardo da Vinci", "Pablo Picasso", "Michelangelo"], "Leonardo da Vinci"),
+    ("What is the chemical symbol for water?", ["H2O", "CO2", "O2", "H2SO4"], "H2O")
+]
